a1ad079b2faf359fdc0d27ceeb27c2eb7cb73b31
ganeshchippada <gany21@gmail.com>
2017-10-03 23:16:56 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# myBot
+# In order to run this project in local
+
+1. Install Node.js. When you download Node.js, you automatically get npm installed on your computer.
+2. npm install 
+3. Use the Bot Framework Emulator to test your bot on localhost.
+4. Install the emulator (https://github.com/Microsoft/BotFramework-Emulator#download) and then start your bot in a console window.
+5. To run app: node app.js
---
+++ app.js
@@ -0,0 +1,44 @@
+const config = require('config')
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+	appId: config.get('application.appConfig.appId'),
+	appPassword: config.get('application.appConfig.appPassword'),
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+var bot = new builder.UniversalBot(connector, function (session) {
+
+/*	var neo4j = require('neo4j');
+	var db = new neo4j.GraphDatabase(config.get('application.neo4j.url'));
+
+	db.cypher({
+	    query: 'MATCH (toPerson:Person) WHERE ANY(name IN toPerson.Name WHERE name =~ \'(?i).*todd.*\') RETURN toPerson',
+	    params: {
+	        userName: 'todd',
+	    },
+	}, function (err, results) {
+	    if (err) throw err;
+	    var result = results[0];
+	    if (!result) {
+	        console.log('No user found.');
+	    } else {
+	    	console.log(JSON.stringify(result, null, 4));
+	        var user = result['toPerson'].properties.Name;
+	        session.send("Did you mean %s?", JSON.stringify(user, null, 4));
+	    }
+	});
+*/
+	
+	session.send("You said: %s", session.message.text);
+});
